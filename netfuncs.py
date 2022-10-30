0243fc6a01a8d4140084488e88ee00941cde38c9
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-10-29 17:40:54 -0700

    designed functions upto the found router for ip. Have yet to test the functions

---
+++ netfuncs.py
@@ -0,0 +1,260 @@
+import sys
+import json
+import socket as s
+import struct
+def ipv4_to_value(ipv4_addr):
+
+    """
+    Convert a dots-and-numbers IP address to a single numeric value.
+
+    Example:
+
+    There is only one return value, but it is shown here in 3 bases.
+
+    ipv4_addr: "255.255.0.0"
+    return:    0xffff0000 0b11111111111111110000000000000000 4294901760
+
+    ipv4_addr: "1.2.3.4"
+    return:    0x01020304 0b00000001000000100000001100000100 16909060
+    """
+
+    byteIP = s.inet_aton(ipv4_addr)
+    return struct.unpack('!L', byteIP)[0]
+
+
+
+
+def value_to_ipv4(addr):
+    """
+    Convert a single 32-bit numeric value to a dots-and-numbers IP
+    address.
+
+    Example:
+
+    There is only one input value, but it is shown here in 3 bases.
+
+    addr:   0xffff0000 0b11111111111111110000000000000000 4294901760
+    return: "255.255.0.0"
+
+    addr:   0x01020304 0b00000001000000100000001100000100 16909060
+    return: "1.2.3.4"
+    """
+
+    return s.inet_ntoa(struct.pack('!L', addr))
+
+def get_subnet_mask_value(slash):
+    """
+    Given a subnet mask in slash notation, return the value of the mask
+    as a single number. The input can contain an IP address optionally,
+    but that part should be discarded.
+
+    Example:
+
+    There is only one return value, but it is shown here in 3 bases.
+
+    slash:  "/16"
+    return: 0xffff0000 0b11111111111111110000000000000000 4294901760
+
+    slash:  "10.20.30.40/23"
+    return: 0xfffffe00 0b11111111111111111111111000000000 4294966784
+    """
+    ip, network_bits = slash.split('/')
+    host = 32 - int(network_bits)
+    mask = s.inet_aton(struct.pack('!L', (1 << 32) - (1 << host)))
+    return mask
+
+def ips_same_subnet(ip1, ip2, slash):
+    """
+    Given two dots-and-numbers IP addresses and a subnet mask in slash
+    notataion, return true if the two IP addresses are on the same
+    subnet.
+
+    FOR FULL CREDIT: this must use your get_subnet_mask_value() and
+    ipv4_to_value() functions. Don't do it with pure string
+    manipulation.
+
+    This needs to work with any subnet from /1 to /31
+
+    Example:
+
+    ip1:    "10.23.121.17"
+    ip2:    "10.23.121.225"
+    slash:  "/23"
+    return: True
+    
+    ip1:    "10.23.230.22"
+    ip2:    "10.24.121.225"
+    slash:  "/16"
+    return: False
+    """
+    ip1_val = ipv4_to_value(ip1)
+    ip2_val = ipv4_to_value(ip2)
+    mask = get_subnet_mask_value(slash)
+    if ip1_val and ip2_val == mask:
+        return True
+    else:
+        return False
+    
+
+def get_network(ip_value, netmask):
+    """
+    Return the network portion of an address value.
+
+    Example:
+
+    ip_value: 0x01020304
+    netmask:  0xffffff00
+    return:   0x01020300
+    """
+    ip = value_to_ipv4(ip_value)
+    ip_bin = ipv4_to_value(ip)
+    net = ip_bin[0:32 -(32-netmask)]
+    return net
+    
+
+def find_router_for_ip(routers, ip):
+    """
+    Search a dictionary of routers (keyed by router IP) to find which
+    router belongs to the same subnet as the given IP.
+
+    Return None if no routers is on the same subnet as the given IP.
+
+    FOR FULL CREDIT: you must do this by calling your ips_same_subnet()
+    function.
+
+    Example:
+
+    [Note there will be more data in the routers dictionary than is
+    shown here--it can be ignored for this function.]
+
+    routers: {
+        "1.2.3.1": {
+            "netmask": "/24"
+        },
+        "1.2.4.1": {
+            "netmask": "/24"
+        }
+    }
+    ip: "1.2.3.5"
+    return: "1.2.3.1"
+
+
+    routers: {
+        "1.2.3.1": {
+            "netmask": "/24"
+        },
+        "1.2.4.1": {
+            "netmask": "/24"
+        }
+    }
+    ip: "1.2.5.6"
+    return: None
+    """
+    
+    
+
+# Uncomment this code to have it run instead of the real main.
+# Be sure to comment it back out before you submit!
+
+def my_tests():
+    print("-------------------------------------")
+    print("This is the result of my custom tests")
+    print("-------------------------------------")
+
+    # Add custom test code here
+
+
+## -------------------------------------------
+## Do not modify below this line
+##
+## But do read it so you know what it's doing!
+## -------------------------------------------
+
+def usage():
+    print("usage: netfuncs.py infile.json", file=sys.stderr)
+
+def read_routers(file_name):
+    with open(file_name) as fp:
+        json_data = fp.read()
+        
+    return json.loads(json_data)
+
+def print_routers(routers):
+    print("Routers:")
+
+    routers_list = sorted(routers.keys())
+
+    for router_ip in routers_list:
+
+        # Get the netmask
+        slash_mask = routers[router_ip]["netmask"]
+        netmask_value = get_subnet_mask_value(slash_mask)
+        netmask = value_to_ipv4(netmask_value)
+
+        # Get the network number
+        router_ip_value = ipv4_to_value(router_ip)
+        network_value = get_network(router_ip_value, netmask_value)
+        network_ip = value_to_ipv4(network_value)
+
+        print(f" {router_ip:>15s}: netmask {netmask}: " \
+            f"network {network_ip}")
+
+def print_same_subnets(src_dest_pairs):
+    print("IP Pairs:")
+
+    src_dest_pairs_list = sorted(src_dest_pairs)
+
+    for src_ip, dest_ip in src_dest_pairs_list:
+        print(f" {src_ip:>15s} {dest_ip:>15s}: ", end="")
+
+        if ips_same_subnet(src_ip, dest_ip, "/24"):
+            print("same subnet")
+        else:
+            print("different subnets")
+
+def print_ip_routers(routers, src_dest_pairs):
+    print("Routers and corresponding IPs:")
+
+    all_ips = sorted(set([i for pair in src_dest_pairs for i in pair]))
+
+    router_host_map = {}
+
+    for ip in all_ips:
+        router = find_router_for_ip(routers, ip)
+        
+        if router not in router_host_map:
+            router_host_map[router] = []
+
+        router_host_map[router].append(ip)
+
+    for router_ip in sorted(router_host_map.keys()):
+        print(f" {router_ip:>15s}: {router_host_map[router_ip]}")
+
+def main(argv):
+    try:
+        my_tests()
+        return 0
+    except NameError as e:
+        if e.name != "my_tests":
+            raise e
+
+    try:
+        router_file_name = argv[1]
+    except:
+        usage()
+        return 1
+
+    json_data = read_routers(router_file_name)
+
+    routers = json_data["routers"]
+    src_dest_pairs = json_data["src-dest"]
+
+    print_routers(routers)
+    print()
+    print_same_subnets(src_dest_pairs)
+    print()
+    print_ip_routers(routers, src_dest_pairs)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+    
